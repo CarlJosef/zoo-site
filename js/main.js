f82b9a5fd6c46af343938b8f6df0eec45f874af5
Carl Josef <jo1333na-s@student.lu.se>
2025-04-24 17:08:12 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Function: showAnimalInfo
+ * Purpose is to show an alert with the animal's name when a card is clicked.
+ * Parameters:
+ * - name (string) = The name of the animal to show in the alert.
+ */
+function showAnimalInfo(name) {
+    alert("You clicked on " + name + "!");
+  }
